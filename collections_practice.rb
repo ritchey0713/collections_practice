5abc250a880e579b5ec9a334103dabef5bee6c62
ritchey0713 <ritchey0713@gmail.com>
2020-12-17 19:49:02 -0500

    commit tree 2dd180487285cff6a63f0f3b92f0be6ce36bbd0a

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
 
+require "pry"
+def sort_array_asc(array)
+  array.sort
+end 
+
+def sort_array_desc(array)
+  array.sort do |left, right|
+    right <=> left
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length 
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array) 
+  array.map do |name| 
+    name[2] = "$"
+    name
+  end 
+end 
+
+def find_a(array)
+  array.find_all {|word| word.start_with?("a")}
+end 
+
+def sum_array(array)
+  # result = 0
+  # array.each {|num| result += num}
+  # result
+  array.reduce(:+)
+end 
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else 
+      word + 's'
+    end
+  end 
+end 
